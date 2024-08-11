e3f50ecc4857282d007085aca7e00cdbaf6f27f5
Kirthick Sakthi V <158608170+KirthickSakthiV@users.noreply.github.com>
2024-08-11 10:27:36 +0530

    Add files via upload

---
+++ String concatenation.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string a = "Kirthick";
+    string b = "Sakthi";
+    string c = a+" "+b;
+    cout<< c;
+    return 0;
+}
